8026e6893df9b64b0d95aea072b5a07ff45bc605
Dillon Williams <dcw679@utexas-cs>
2012-06-11 19:02:53 -0500

    dcw679 assertion and unit tests

---
+++ dcw679-TestCollatz.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+
+# -------------------------------
+# projects/collatz/TestCollatz.py
+# Copyright (C) 2011
+# Glenn P. Downing
+# -------------------------------
+
+"""
+To test the program:
+    % python TestCollatz.py > TestCollatz.py.out
+    % chmod ugo+x TestCollatz.py
+    % TestCollatz.py > TestCollatz.py.out
+"""
+
+# -------
+# imports
+# -------
+
+import StringIO
+import unittest
+
+from Collatz import collatz_read, collatz_eval, collatz_print, collatz_solve, collatz_cycle
+
+# -----------
+# TestCollatz
+# -----------
+
+class TestCollatz (unittest.TestCase) :
+    # ----
+    # read
+    # ----
+
+    def test_read (self) :
+        r = StringIO.StringIO("1 10\n")
+        a = [0, 0]
+        b = collatz_read(r, a)
+        self.assert_(b    == True)
+        self.assert_(a[0] ==  1)
+        self.assert_(a[1] == 10)
+
+    # ----
+    # eval
+    # ----
+
+    def test_eval_1 (self) :
+        v = collatz_eval(1, 10)
+        self.assert_(v == 20)
+
+    def test_eval_2 (self) :
+        v = collatz_eval(100, 200)
+        self.assert_(v == 125)
+
+    def test_eval_3 (self) :
+        v = collatz_eval(201, 210)
+        self.assert_(v == 89)
+
+    def test_eval_4 (self) :
+        v = collatz_eval(900, 1000)
+        self.assert_(v == 174)
+
+    def test_eval_5 (self) :
+        v = collatz_eval(200, 100)
+        self.assert_(v == 125)
+    
+    def test_eval_6 (self) :
+        v = collatz_eval(5, 5)
+        self.assert_(v == 6)
+    # ----
+    # cycle
+    # ----
+    
+    def test_cycle_1 (self) :
+        v = collatz_cycle(1)
+        self.assert_(v == 1)
+ 
+    def test_cycle_2 (self) :
+        v = collatz_cycle(2) 
+        self.assert_(v == 2)
+
+    def test_cycle_3 (self) :
+        v = collatz_cycle(27)
+        self.assert_(v == 112)
+
+    # -----
+    # print
+    # -----
+
+    def test_print (self) :
+        w = StringIO.StringIO()
+        collatz_print(w, 1, 10, 20)
+        self.assert_(w.getvalue() == "1 10 20\n")
+
+    # -----
+    # solve
+    # -----
+
+    def test_solve (self) :
+        r = StringIO.StringIO("1 10\n100 200\n201 210\n900 1000\n")
+        w = StringIO.StringIO()
+        collatz_solve(r, w)
+        self.assert_(w.getvalue() == "1 10 20\n100 200 125\n201 210 89\n900 1000 174\n")
+
+# ----
+# main
+# ----
+
+print "TestCollatz.py"
+unittest.main()
+print "Done."
